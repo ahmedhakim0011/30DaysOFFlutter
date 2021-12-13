728f44f439512642c060cd768a8ef7aac05d1806
Ahmed Hakim Ali <ahmdahmed259@gmail.com>
2021-12-13 19:20:12 +0500

    Da13 completed, we learned get data from json file

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,30 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, unused_local_variable
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_catlog/models/catalog.dart';
 import 'package:flutter_catlog/widgets/drawer.dart';
 import 'package:flutter_catlog/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "Codepure";
 
   final dummyList = List.generate(20, (index) => CatalogMaodel.items[0]);
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,5 +45,13 @@ class HomePage extends StatelessWidget {
       ),
       drawer: MyDrawer(),
     );
+  }
+
+  void loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/cataloJson.json");
+    final decodeData = jsonDecode(catalogJson);
+    var productsData = decodeData["products"];
+    print(productsData);
   }
 }
